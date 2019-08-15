4510ca9d9ee8b99b485fba5c4dd5eaaa8e7b79c5
Cartoon21014paphawarin <51435084+Cartoon21014paphawarin@users.noreply.github.com>
2019-08-15 13:02:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if (userText == 'ไปเถอะ') :
-        sendText(user,'ไปไหนอ้ะ')
+    if (userText == 'สวัสดี') :
+        sendText(user,'เออดีด้วย')
     elif (userText == 'ไปนะ') :
-        sendText(user,'ชวนจิ๊งง')
+        sendText(user,'ไปตายที่ก็ไป')
     else :
         sendText(user,'ว่าไงนะ')
 
